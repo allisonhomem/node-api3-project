aacee7147392c848aea724348b0f28e4e29ebda9
Allison Homem <mrs.allisonhomem@outlook.com>
2021-09-29 10:19:31 -0500

    logger middleware working

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,28 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  
+  console.log(`The method ${method} was used at the url ${url} at the time ${timestamp}`);
+
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  
+  next();
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  
+  next();
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  
+  next();
 }
 
-// do not forget to expose these functions to other modules
+
+//export
+module.exports = {logger, validateUserId, validateUser, validatePost};
--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 //imports
 const express = require('express');
 const usersRouter = require('./users/users-router.js');
+const {logger, validateUserId, validateUser, validatePost} = require('./middleware/middleware.js');
 
 const server = express();
 
 
 server.use(express.json());
-
+server.use(logger, validateUserId, validateUser, validatePost);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
